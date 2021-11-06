0f492b9b7e571015a2d99988377e246ef130f9a7
Aliaksandr Bulynka <abulynka@gmail.com>
2021-11-07 01:35:07 +0300

    feat: add solution for 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,35 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
+const { EOL } = require('os');
+
+async function merge(fromDirectory, toFile) {
+  fromDirectory = path.normalize(fromDirectory);
+  toFile = path.normalize(toFile);
+
+  try {
+    await fsPromises.stat(toFile);
+    await fsPromises.rm(toFile);
+  } catch (e) {
+    // nothing to do
+  }
+
+  for (const element of (await fsPromises.readdir(fromDirectory, {withFileTypes: true}))) {
+    const from = path.normalize(fromDirectory + '/' + element.name);
+
+    if (element.isFile() === false) {
+      continue;
+    }
+
+    const extension = path.parse(element.name).ext.split('.').join('');
+    if (extension !== 'css') {
+      continue;
+    }
+
+    await fsPromises.appendFile(
+      toFile,
+      await fsPromises.readFile(from) + EOL
+    );
+  }
+}
+
+merge(__dirname + '/styles', __dirname + '/project-dist/bundle.css').then();
